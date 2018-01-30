8f481501b759a53b07ec7a582eeb9b9cc1ff8d45
Henry Chukwu <henry.chukwu@andela.com>
2018-01-30 13:00:56 +0100

    initial project setup with login validation, authorization and bootstrap form

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDsT9mTtHQoGpCqEBRo1j6eCFJk7SUjXnk",
+    authDomain: "shop-allkoboz.firebaseapp.com",
+    databaseURL: "https://shop-allkoboz.firebaseio.com",
+    projectId: "shop-allkoboz",
+    storageBucket: "shop-allkoboz.appspot.com",
+    messagingSenderId: "359717571155"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDsT9mTtHQoGpCqEBRo1j6eCFJk7SUjXnk",
+    authDomain: "shop-allkoboz.firebaseapp.com",
+    databaseURL: "https://shop-allkoboz.firebaseio.com",
+    projectId: "shop-allkoboz",
+    storageBucket: "shop-allkoboz.appspot.com",
+    messagingSenderId: "359717571155"
+  }
 };
