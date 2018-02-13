edf96efcf28e9ebbc008cc10517d335b009292d3
Henry Chukwu <henry.chukwu@andela.com>
2018-02-13 13:31:23 +0100

    work in progress

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -2,7 +2,7 @@ import { Product } from './../../models/product';
 import { ProductService } from './../../product.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
-// import { DataTableResource } from 'angular-4-data-table';
+import { DataTable, DataTableResource } from 'angular-4-data-table-fix';
 
 @Component({
   selector: 'app-admin-products',
@@ -11,40 +11,41 @@ import { Subscription } from 'rxjs/Subscription';
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
   products: Product[];
-  filteredProducts: any[];
   subscription: Subscription;
-  // tableResource: DataTableResource<Product>;
+  tableResource: DataTableResource<Product>;
   items: Product[] = [];
   itemCount: number;
 
   constructor(private productService: ProductService) {
     this.subscription = this.productService.getAll()
       .subscribe(products => {
-        this.filteredProducts = this.products = products;
+        this.products = products;
 
-        // this.initializeTable(products);
+        this.initializeTable(products);
       });
   }
 
-  // private initializeTable(products: Product[]) {
-  //   this.tableResource = new DataTableResource(products);
-  //   this.tableResource.query({ offset: 0 })
-  //     .then(items => this.items = items);
-  //   this.tableResource.count()
-  //     .then(count => this.itemCount = count);
-  // }
+  private initializeTable(products: Product[]) {
+    this.tableResource = new DataTableResource(products);
+    this.tableResource.query({ offset: 0 })
+      .then(items => this.items = items);
+    this.tableResource.count()
+      .then(count => this.itemCount = count);
+  }
 
-  // reloadItems(params) {
-  //   if (!this.tableResource) return;
+  reloadItems(params) {
+    if (!this.tableResource) return;
 
-  //   this.tableResource.query(params)
-  //   .then(items => this.items = items);
-  // }
+    this.tableResource.query(params)
+    .then(items => this.items = items);
+  }
 
   filter(query: string) {
-    this.filteredProducts = (query) ?
+    let filteredProducts = (query) ?
       this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
       this.products;
+
+    this.initializeTable(filteredProducts);
   }
 
   ngOnDestroy() {
