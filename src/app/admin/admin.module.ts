dac69a4dfe6ea86fabd14f7a4cb8492f75f1ff93
Henry Chukwu <henry.chukwu@andela.com>
2018-02-23 17:38:07 +0100

    modularize the project

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,48 @@
+import { DataTableModule } from 'angular-4-data-table-fix';
+import { SharedModule } from './../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AdminOrdersComponent } from 'app/admin/components/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from 'app/admin/components/admin-products/admin-products.component';
+import { ProductFormComponent } from 'app/admin/components/product-form/product-form.component';
+import { AdminAuthGuard } from 'app/admin/services/admin-auth-guard.service';
+import { RouterModule } from '@angular/router';
+import { AuthGuard } from 'shared/services/auth-guard.service';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild([
+       {
+        path: 'admin/products/new',
+        component: ProductFormComponent,
+        canActivate: [AuthGuard, AdminAuthGuard]
+       },
+       {
+        path: 'admin/products/:id',
+        component: ProductFormComponent,
+        canActivate: [AuthGuard, AdminAuthGuard]
+       },
+       {
+        path: 'admin/products',
+        component: AdminProductsComponent,
+        canActivate: [AuthGuard, AdminAuthGuard]
+       },
+      {
+        path: 'admin/orders',
+        component: AdminOrdersComponent,
+        canActivate: [AuthGuard, AdminAuthGuard]
+      }
+    ])
+  ],
+  declarations: [
+    ProductFormComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+  ],
+  providers: [
+    AdminAuthGuard
+  ]
+})
+export class AdminModule { }
